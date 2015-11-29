23e731dc7bbf3b0464b3d3d29b07c3afff11c62b
Naomi Guyer <adrexia@gmail.com>
2015-11-29 15:54:41 +1300

    Change dataformat

--- code/events/EventHelper.php
+++ code/events/EventHelper.php
@@ -2,45 +2,45 @@
 /**
  * Event Helper
  * Helper class for event related calculations and formatting
- * 
+ *
  * @package calendar
  */
 class EventHelper {
 
-	
+
 	/**
 	 * Formatted Dates
 	 * Returns either the event's date or both start and end date if the event spans more than
 	 * one date
-	 * 
+	 *
 	 * Format:
 	 * Jun 7th - Jun 10th
-	 * 
+	 *
 	 * @param SS_Datetime $startObj
 	 * @param SS_Datetime $endObj
 	 * @return string
 	 */
 	static function formatted_dates($startObj, $endObj){
-		
+
 		//Checking if end date is set
 		$endDateIsset = true;
 		if (isset($endObj->value)) {
 			$endDateIsset = false;
 		}
-		
+
 		$startTime = strtotime($startObj->value);
 		$endTime = strtotime($endObj->value);
-		
+
 		$startMonth = date('M', $startTime);
 		$startDayOfMonth = $startObj->DayOfMonth(true);
 		$str = $startMonth . ' ' . $startDayOfMonth;
-		
+
 		if (date('Y-m-d', $startTime) == date('Y-m-d', $endTime)) {
 			//one date - str. has already been written
-			
+
 		} else {
 			//two dates
-			
+
 			if ($endDateIsset) {
 				$endMonth = date('M', $endTime);
 				$endDayOfMonth = $endObj->DayOfMonth(true);
@@ -50,7 +50,7 @@ class EventHelper {
 				} else {
 					$str .= ' - ' . $endMonth . ' ' . $endDayOfMonth;
 				}
-				
+
 			}
 		}
 		return $str;
@@ -69,20 +69,20 @@ class EventHelper {
 		$endTime = strtotime($endObj->value);
 
 		if (date('g:ia', $startTime) == '12:00am') {
-			$startDate = date('j F, Y', $startTime);
+			$startDate = date('d M Y', $startTime);
 		} else {
-			$startDate = date('j F, Y (g:ia)', $startTime);
+			$startDate = date('d M Y (g:ia)', $startTime);
 		}
 
 		if (date('g:ia', $endTime) == '12:00am') {
-			$endDate = date('j F, Y', $endTime);
+			$endDate = date('d M Y', $endTime);
 		} else {
-			$endDate = date('j F, Y (g:ia)', $endTime);
+			$endDate = date('d M Y (g:ia)', $endTime);
 		}
 
 		return $startDate." &ndash; ".$endDate;
 	}
-	
+
 	/**
 	 * Formatted time frame
 	 * Returns either a string or null
@@ -94,11 +94,11 @@ class EventHelper {
 	static function formatted_timeframe($startStr, $endStr) {
 
 		$str = null;
-		
+
 		if ($startStr == $endStr) {
 			return null;
 		}
-		
+
 		$startTime = strtotime($startStr->value);
 		$endTime = strtotime($endStr->value);
 
@@ -110,9 +110,9 @@ class EventHelper {
 		} else {
 			$str = date('g:ia', $startTime);
 		}
-		
+
 		return $str;
 	}
-	
-	
+
+
 }
