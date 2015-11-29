360d94dfab07acbac9d182321932debafc64aa47
Naomi Guyer <adrexia@gmail.com>
2015-11-29 15:54:41 +1300

    Change dataformat

--- code/events/EventHelper.php
+++ code/events/EventHelper.php
@@ -5,116 +5,114 @@
  *
  * @package calendar
  */
-class EventHelper
-{
+class EventHelper {
 
 
-    /**
-     * Formatted Dates
-     * Returns either the event's date or both start and end date if the event spans more than
-     * one date
-     *
-     * Format:
-     * Jun 7th - Jun 10th
-     *
-     * @param SS_Datetime $startObj
-     * @param SS_Datetime $endObj
-     * @return string
-     */
-    public static function formatted_dates($startObj, $endObj)
-    {
+	/**
+	 * Formatted Dates
+	 * Returns either the event's date or both start and end date if the event spans more than
+	 * one date
+	 *
+	 * Format:
+	 * Jun 7th - Jun 10th
+	 *
+	 * @param SS_Datetime $startObj
+	 * @param SS_Datetime $endObj
+	 * @return string
+	 */
+	static function formatted_dates($startObj, $endObj){
 
-        //Checking if end date is set
-        $endDateIsset = true;
-        if (isset($endObj->value)) {
-            $endDateIsset = false;
-        }
+		//Checking if end date is set
+		$endDateIsset = true;
+		if (isset($endObj->value)) {
+			$endDateIsset = false;
+		}
 
-        $startTime = strtotime($startObj->value);
-        $endTime = strtotime($endObj->value);
+		$startTime = strtotime($startObj->value);
+		$endTime = strtotime($endObj->value);
 
-        $startMonth = date('M', $startTime);
-        $startDayOfMonth = $startObj->DayOfMonth(true);
-        $str = $startMonth . ' ' . $startDayOfMonth;
+		$startMonth = date('M', $startTime);
+		$startDayOfMonth = $startObj->DayOfMonth(true);
+		$str = $startMonth . ' ' . $startDayOfMonth;
 
-        if (date('Y-m-d', $startTime) == date('Y-m-d', $endTime)) {
-            //one date - str. has already been written
-        } else {
-            //two dates
+		if (date('Y-m-d', $startTime) == date('Y-m-d', $endTime)) {
+			//one date - str. has already been written
 
-            if ($endDateIsset) {
-                $endMonth = date('M', $endTime);
-                $endDayOfMonth = $endObj->DayOfMonth(true);
+		} else {
+			//two dates
 
-                if ($startMonth == $endMonth) {
-                    $str .= ' - ' . $endDayOfMonth;
-                } else {
-                    $str .= ' - ' . $endMonth . ' ' . $endDayOfMonth;
-                }
-            }
-        }
-        return $str;
-    }
+			if ($endDateIsset) {
+				$endMonth = date('M', $endTime);
+				$endDayOfMonth = $endObj->DayOfMonth(true);
 
-    public static function formatted_alldates($startObj, $endObj)
-    {
-        $startDate = date("Y-m-d", strtotime($startObj->value));
-        $endDate = date("Y-m-d", strtotime($endObj->value));
+				if ($startMonth == $endMonth) {
+					$str .= ' - ' . $endDayOfMonth;
+				} else {
+					$str .= ' - ' . $endMonth . ' ' . $endDayOfMonth;
+				}
 
-        if ($startDate == $endDate) {
-            return false;
-        }
+			}
+		}
+		return $str;
+	}
 
-        $startTime = strtotime($startObj->value);
-        $endTime = strtotime($endObj->value);
+	static function formatted_alldates($startObj, $endObj){
 
-        if (date('g:ia', $startTime) == '12:00am') {
-            $startDate = date('j F, Y', $startTime);
-        } else {
-            $startDate = date('j F, Y (g:ia)', $startTime);
-        }
+		$startDate = date("Y-m-d", strtotime($startObj->value));
+		$endDate = date("Y-m-d", strtotime($endObj->value));
 
-        if (date('g:ia', $endTime) == '12:00am') {
-            $endDate = date('j F, Y', $endTime);
-        } else {
-            $endDate = date('j F, Y (g:ia)', $endTime);
-        }
+		if ($startDate == $endDate) {
+			return false;
+		}
 
-        return $startDate." &ndash; ".$endDate;
-    }
+		$startTime = strtotime($startObj->value);
+		$endTime = strtotime($endObj->value);
 
-    /**
-     * Formatted time frame
-     * Returns either a string or null
-     * Time frame is only applicable if both start and end time is on the same day
-     * @param string $startStr
-     * @param string $endStr
-     * @return string|null
-     */
-    public static function formatted_timeframe($startStr, $endStr)
-    {
-        $str = null;
+		if (date('g:ia', $startTime) == '12:00am') {
+			$startDate = date('d M Y', $startTime);
+		} else {
+			$startDate = date('d M Y (g:ia)', $startTime);
+		}
 
-        if ($startStr == $endStr) {
-            return null;
-        }
+		if (date('g:ia', $endTime) == '12:00am') {
+			$endDate = date('d M Y', $endTime);
+		} else {
+			$endDate = date('d M Y (g:ia)', $endTime);
+		}
 
-        $startTime = strtotime($startStr->value);
-        $endTime = strtotime($endStr->value);
+		return $startDate." &ndash; ".$endDate;
+	}
 
-        if ($startTime == $endTime) {
-            return null;
-        }
+	/**
+	 * Formatted time frame
+	 * Returns either a string or null
+	 * Time frame is only applicable if both start and end time is on the same day
+	 * @param string $startStr
+	 * @param string $endStr
+	 * @return string|null
+	 */
+	static function formatted_timeframe($startStr, $endStr) {
 
-        if ($endStr) {
-            //time frame is only applicable if both start and end time is on the same day
-            if (date('Y-m-d', $startTime) == date('Y-m-d', $endTime)) {
-                $str = date('g:ia', $startTime) . ' - ' . date('g:ia', $endTime);
-            }
-        } else {
-            $str = date('g:ia', $startTime);
-        }
+		$str = null;
 
-        return $str;
-    }
+		if ($startStr == $endStr) {
+			return null;
+		}
+
+		$startTime = strtotime($startStr->value);
+		$endTime = strtotime($endStr->value);
+
+		if ($endStr) {
+			//time frame is only applicable if both start and end time is on the same day
+			if (date('Y-m-d', $startTime) == date('Y-m-d', $endTime)) {
+				$str = date('g:ia', $startTime) . ' - ' . date('g:ia', $endTime);
+			}
+		} else {
+			$str = date('g:ia', $startTime);
+		}
+
+		return $str;
+	}
+
+
 }
